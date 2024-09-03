142dcebab8ceddd82b14af44cbcd86fb99ddabbe
Akashbhasme0990 <akashbhasme011@gmail.com>
2024-09-03 21:13:53 +0530

    Spring security in progress

---
+++ src/main/java/net/engineeringdigest/journalApp/config/SpringSecurity.java
@@ -0,0 +1,9 @@
+package net.engineeringdigest.journalApp.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+@Deprecated
+@EnableWebSecurity
+public class SpringSecurity extends WebSecurityConfigurerAdapter {
+
+}
--- src/main/java/net/engineeringdigest/journalApp/repository/UserRepository.java
+++ src/main/java/net/engineeringdigest/journalApp/repository/UserRepository.java
@@ -1,5 +1,4 @@
 package net.engineeringdigest.journalApp.repository;
-
 import net.engineeringdigest.journalApp.entity.User;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;

e61424269e02df84d7ad058ba2541cb6623b043e
Akashbhasme0990 <akashbhasme011@gmail.com>
2024-11-18 23:53:05 +0530

    first commit (testing)

--- src/main/java/net/engineeringdigest/journalApp/Utils/JwtUtil.java
+++ src/main/java/net/engineeringdigest/journalApp/Utils/JwtUtil.java
@@ -1,12 +1,9 @@
 package net.engineeringdigest.journalApp.Utils;
-
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
-
 import javax.crypto.SecretKey;
-import javax.swing.*;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
